5ea05033a58efb0d39b9c51a4c4ca02c44e9e303
Mizanur Rahaman <mijan@spickolab.net>
2022-10-23 21:53:46 +0600

    server

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,13 +1,53 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const TodoPage = () => {
+
+
+
+  const [value,setValue] = useState("");
+  
+  const [todos,setTodos] = useState([]);
+
+
+  const addNote = () => {
+    todos.push(value);
+  }
+
+  const inputValueChange = (event) => {
+    setValue(event.target.value);
+  }
+
+
+  
   return (
     <div>
-      <div>TodoPage</div>
-    {/* <Link to="contact">About Us</Link> */}
-    </div>
-  )
-}
+      <div className="w-full flex flex-col justify-center items-center">
+        {/* form  */}
+        <h1>{value}</h1>        
+        <div className="w-80 bg-slate-600 flex">
+          <input
+            className="text-black flex-1 p-3"
+            type="text"
+            placeholder="write a note"
+            defaultValue={value}
+            onChange={inputValueChange}
+          />
+          <button onClick={addNote} className="p-3">Add</button>
+        </div>
+        {/* list */}
+        <div className="w-80 h-[450px] bg-slate-500">
 
-export default TodoPage
+          {
+            todos.map((item,index)=>{
+              return <div key={index}>{item}</div>
+            })
+          }
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TodoPage;
